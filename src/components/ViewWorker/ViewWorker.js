7a1fe0b101543916895395d8dfd32f48f4bee488
Darren Dawson <dwdawson@ucsc.edu>
2018-11-04 06:06:31 -0800

    Add reviews and experience to <ViewWorker/>

--- src/components/ViewWorker/ViewWorker.js
+++ src/components/ViewWorker/ViewWorker.js
@@ -7,6 +7,14 @@ class ViewWorker extends Component {
   constructor() {
     super();
 
+    let review1 = {title: "title", company: "company", "score": " ★ ★ ★ ☆ ☆ ", content: "this is the review!"};
+    let review2 = {title: "t2", company: "c2", score: " ★ ★ ★ ★ ☆ ", content: "review #2"};
+    let reviews = [review1, review2];
+
+    let experience1 = {title: "job title", company: "company name", description: "description of what was done"};
+    let experience2 = {title: "job title #2", company: "company name #2", description: "description #2"};
+    let experience = [experience1, experience2];
+
     let worker = {
       name: "Darren Dawson",
       bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -14,7 +22,9 @@ class ViewWorker extends Component {
       skills: ["skill 1", "skill 2"],
       profile_img: "https://avatars1.githubusercontent.com/u/12383469?s=400&v=4",
       num_jobs: "15 successful jobs",
-      video_id: "2g811Eo7K8U"
+      video_id: "2g811Eo7K8U",
+      reviews: reviews,
+      experience: experience
     };
 
 
@@ -90,6 +100,12 @@ class ViewWorker extends Component {
   renderReview = (review) => {
     return (
       <div className="vw-review-container">
+        <div className="vw-review-top-bar">
+          <h1 className="vw-review-title">{review.title} - {review.company}</h1>
+          <h1 className="vw-review-stars">{review.score}</h1>
+        </div>
+
+        <p className="vw-review-p">{review.content}</p>
 
       </div>
     );
@@ -97,11 +113,48 @@ class ViewWorker extends Component {
 
   // render reviews
   renderReviewsBar = () => {
-    let review = {title: "title", company: "company", "score": " * * * * *"};
 
+    let results = [];
+
+    let reviews = this.state.worker.reviews;
+    for (let i = 0; i < reviews.length; i++) {
+      results.push(this.renderReview(reviews[i]));
+    }
     return (
       <div className="vw-reviews-bar-container">
-        {this.renderReview(review)}
+        <h1 id="review-title">Reviews</h1>
+        {results}
+      </div>
+    );
+  }
+
+
+
+  renderExperience = (experience) => {
+    return (
+      <div className="vw-review-container">
+        <div className="vw-review-top-bar">
+          <h1 className="vw-review-title">{experience.title} - {experience.company}</h1>
+        </div>
+
+        <p className="vw-review-p">{experience.description}</p>
+      </div>
+    );
+  }
+
+
+  renderExperienceBar = () => {
+
+    let results = [];
+    let experience = this.state.worker.experience;
+    for (let i = 0; i < experience.length; i++) {
+      results.push(this.renderExperience(experience[i]));
+    }
+
+    return (
+      <div className="vw-experience-bar-container">
+        <h1 id="review-title">Experience</h1>
+        {results}
       </div>
     );
   }
@@ -111,13 +164,16 @@ class ViewWorker extends Component {
     return (
       <div className="vw-full-container">
         <div className="vw-side-column" onClick={this.props.closeViewWorker}></div>
-        <div className="vw-column-container">
-          <div className="vw-top-gray"></div>
-          {this.renderTopBar()}
-          {this.renderVideoBar()}
-          {this.renderReviewsBar()}
-
+        <div className="vw-column-scroll-container">
+          <div className="vw-column-container">
+            <div className="vw-top-gray"></div>
+            {this.renderTopBar()}
+            {this.renderVideoBar()}
+            {this.renderReviewsBar()}
+            {this.renderExperienceBar()}
+          </div>
         </div>
+
         <div className="vw-side-column" onClick={this.props.closeViewWorker}></div>
       </div>
     );
